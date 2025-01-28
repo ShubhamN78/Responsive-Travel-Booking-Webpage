6b81808d7892310a82a249c2a80b077bbb09f49b
SHUBHAM <shubhamnatkar97#gmail.com>
2025-01-28 18:29:53 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    const menuIcon = document.getElementById("menu-icon");
+    const navList = document.getElementById("nav-list");
+
+    menuIcon.addEventListener("click", () => {
+        navList.classList.toggle("active");
+    });
+</script>
